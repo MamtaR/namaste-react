89ef421505e758abd4d6dc3d7433f7b7019b7ad3
Mamta <Mamta.X.Ramrakhyani@kp.org>
2023-06-02 15:02:00 +0530

    jsx understanding - episode 3

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 // const heading = React.createElement("h1", {
 //     id: "heading"
 // }, "Hello from React");
@@ -22,16 +24,34 @@
  */
 
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "Hello I am h1 tag"),
-    React.createElement("h2", {}, "Hello I am h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "Hello I am h1 tag"),
-    React.createElement("h2", {}, "Hello I am h2 tag"),
-  ]),
-]);
+// const parent = React.createElement("div", { id: "parent" }, [
+//   React.createElement("div", { id: "child1" }, [
+//     React.createElement("h1", {}, "Hello I am h1 tag"),
+//     React.createElement("h2", {}, "Hello I am h2 tag"),
+//   ]),
+//   React.createElement("div", { id: "child2" }, [
+//     React.createElement("h1", {}, "Hello I am h1 tag"),
+//     React.createElement("h2", {}, "Hello I am h2 tag"),
+//   ]),
+// ]);
+
+// React Element
+const jsxHeading = <h1 className="head">Hello from React app Element</h1>;
+
+//React Functional Component
+const HeadingComponent = () => {
+  return <h1 className="head">Hello from React app</h1>;
+};
+
+const HeadingComponentWithoutReturn = () => (
+    <div className="container">
+      {jsxHeading}
+   <h1 className="header">Hello from React app headingComponentWithoutReturn</h1>
+   </div>
+); 
+
+console.log(jsxHeading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<><HeadingComponent /><HeadingComponentWithoutReturn /></>
+);
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# Namaste React Learnings
+#  Namaste React Learnings
+
+
+# Parcel
+- website to read about parcel - https://parceljs.org/
+- creates a dev build
+- creates a local server
+- Its an HMR - Hot Module Replacement (meaning that it will refreshes the server)
+- it uses the file watching alogrithm - written in C++
+- it caches the data - which will help in FASTER BUILDS
+- Image Optimization
+- Minify the files
+- It also acts like a bundler
+- helps compressing the files
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - to support older browsers
+- good Error Handling
+- HTTPs
+- Tree Shaking - will remove unused code for you
+- Different build for dev and production
+
+# cmd to do dev build - npx parcel index.html
+
+# cmd to do prod build - npx parcel index.html
